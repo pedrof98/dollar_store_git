0b959e77f7ea62ed83db037712d9511aee459cf8
pvieira <pvieira@optaresolutions.com>
2024-12-22 20:35:37 +0100

    Finished object and commit functionalities

--- libwyag.py
+++ libwyag.py
@@ -327,7 +327,7 @@ def object_read(repo, sha):
             raise Exception("Malformed object {0}: bad length".format(sha))
 
         #Pick constructor
-        math fmt:
+        match fmt:
             case b'commit'  : c=GitCommit
             case b'tree'    : c=GitTree
             case b'tag'     : c=GitTag
@@ -368,15 +368,250 @@ def object_write(obj, repo=None):
 
 
 
+# Let's get into blobs
+
+"""
+Blobs are the simplest of the four types of objects (blob, commit, tag, tree), because they have no actual format.
+
+Blobs are user data:
+    the content of every file you put in git (main.c, logo.png, RADME.md) is stored as a blob.
+    That makes them easy to manipulate, because they have no actual syntax or contraints beyond the basic object storage mechanism.
+
+Creating a GitBlob class is thus trivial, the serialize and deserialize functions just have to store and return their input unmodified
+"""
+
+class GitBlob(GitObject):
+    fmt=b'blob'
+
+    def serialize(self):
+        return self.blobdata
+
+    def deserialize(self, data):
+        self.blobdata = data
+
+
+"""
+Let's now create the cat-file command to simply print the raw contents of an object to stdout.
+(uncompressed without the git header)
+"""
+
+# Here is the subparser
+
+argsp = argsubparsers.add_parser("cat-file", help="Provide content of repository objects")
+
+argsp.add_argument("type",
+                    metavar="type",
+                    choices=["blob", "commit", "tag", "tree"],
+                    help="Specify the type")
+
+argsp.add_argument("object", metavar="object", help="The object to display")
+
+
+def cmd_cat_file(args):
+    repo = repo_find()
+    cat_file(repo, args.object, fmt=args.type.encode())
+
+def cat_file(repo, obj, fmt=None):
+    obj = object_read(repo, object_find(repo, obj, fmt=fmt))
+    sys.stdout.buffer.write(obj.serialize())
+
+
+def object_find(repo, name, fmt=None, follow=True):
+    return name
+#This is just a temporary placeholder
+
+
+# Hash-object command
+
+argsp = argsubparsers.add_parser(
+        "hash-object",
+        help="Compute object ID and optionally creates a blob from a file")
+
+argsp.add_argument("-t",
+                    metavar="type",
+                    dest="type",
+                    choices=["blob", "commit", "tag", "tree"],
+                    default="blob",
+                    help="Specify the type")
+
+argsp.add_argument("-w",
+                    dest="write",
+                    action="store_true",
+                    help="Actually write the object into the database")
+
+argsp.add_argument("path", help="Read object from <file>")
+
+
+def cmd_hash_object(args):
+    if args.write:
+        repo = repo_find()
+    else:
+        repo = None
+
+    with open(args.path, "rb") as fd:
+        sha = object_hash(fd, args.type.encode(), repo)
+        print(sha)
+
+
+
+def object_hash(fd, fmt, repo=None):
+    """Hash object, writing it to repo if provided"""
+    data = fd.read()
+
+    #Choose contructor according to fmt argument
+    match fmt:
+        case b'commit'  : obj=GitCommit(data)
+        case b'tree'    : obj=GitTree(data)
+        case b'tag'     : obj=GitTag(data)
+        case b'blob'    : obj=GitBlob(data)
+        case _:
+            raise Exception("Unknown type %s!" % fmt)
+
+    return object_write(obj, repo)
+
+
+
+# Parsing commits
+
+"""
+We start by writing a simple parser for the format.
+kvlm means key-value list with message and will be used to parse the commit
+"""
+
+def kvlm_parse(raw, start=0, dct=None):
+    if not dct:
+        dct = collections.OrderedDict()
+        # You cannot declare the argument as dct=OrderedDict() ot all calls to the function will endlessly grow the same dict
+
+    # This function is recursive: it reads a key/value pair, then calls
+    # itself back with the new position. So we first need to know where we are: at a keyword, or already in the messageQ
+
+    # We search for the next space and the next newline
+    spc = raw.find(b' ', start)
+    nl = raw.find(b'\n', start)
+
+    """
+    If space appears before newline, we have a keyword.
+    Otherwise, it's the final message, which we just read to the end of the file.
+
+    Base case
+    ===========
+    If newline appears first (or there's no space at all, in which case "find" returns -1, we assume a blank line.
+    A blank line means the remainder of the data is the message.
+    We store it in the dictionary, with None as the key, and return.
+    """
+    if (spc < 0) or (nl > spc):
+        assert nl == start
+        dct[None] = raw[start+1:]
+        return dct
+
+    # Recursive case: we read a key-value pair and recurse for the next
+    key = raw[start:spc]
+
+    # Find the end of the value. Continuation lines begin with a space, so we loop until we find a "\n" not followed by a space.
+    end = start
+    while True:
+        end = raw.find(b'\n', end+1)
+        if raw[end+1] != ord(' '): break
+
+    # Grab the vlue and drop the leading space on continuation lines
+    value = raw[spc+1:end].replace(b'\n ', b'\n')
+
+    # Don't overwrite existing data contents
+    if key in dct:
+        if type(dct[key]) == list:
+            dct[key].append(value)
+        else:
+            dct[key] = [ dct[key], value ]
+    else:
+        dct[key]=value
+
+    return kvlm_parse(raw, start=end+1, dct=dct)
 
 
 
 
+def kvlm_serialize(kvlm):
+    ret = b''
+
+    # Output fields
+    for k in kvlm.keys():
+        # skip the message itself
+        if k == None: continue
+        val = kvlm[k]
+        # normalize to a list
+        if type(val) != list:
+            val = [ val ]
+
+        for v in val:
+            ret += k + b' ' + (v.replace(b'\n', b'\n ')) + b'\n'
+
+    # Append message
+    ret += b'\n' + kvlm[None] + b'\n'
+
+    return ret
 
 
+# The commit object
+
+class GitCommit(GitObject):
+    fmt=b'commit'
+
+    def deserialize(self, data):
+        self.kvlm = kvlm_parse(data)
+
+    def serialize(self):
+        return kvlm_serialize(self.kvlm)
+
+    def init(self):
+        self.kvlm = dict()
 
 
+# The log command -- simplified
+
+argsp = argsubparsers.add_parser("log", help="Display history of a given commit")
+argsp.add_argument("commit", default="HEAD", nargs="?", help="Commit to start at.")
 
 
+def cmd_log(args):
+    repo = repo_find()
+
+    print("diagraph wyaglog{")
+    print(" node[shape=rect]")
+    log_graphviz(repo, object_find(repo, args.commit), set())
+    print("}")
+
+
+def log_graphviz(repo, sha, seen):
+
+    if sha in seen:
+        return
+    seen.add(sha)
+
+    commit = object_read(repo, sha)
+    short_hash = sha[0:8]
+    message = commit.kvlm[None].decode("utf-8").strip()
+    message = message.replace("\\", "\\\\")
+    message = message.replace("\"", "\\\"")
+
+    if "\n" in message: # Keep only the first line
+        message = message[:message.index("\n")]
+
+    print(" c_{0} [label=\"{1}: {2}\"]".format(sha, sha[0:7], message))
+    assert commit.fmt==b'commit'
+
+    if not b'parent' in commit.kvlm.keys():
+        # Base case: the initial commit
+        return
+
+    parents = commit.kvlm[b'parent']
+
+    if type(parents) != list:
+        parents = [ parents ]
+
+    for p in parents:
+        p = p.decode("ascii")
+        print (" c_{0} -> c_{1};".format(sha, p))
+        log_graphviz(repo, p, seen)
 
 
